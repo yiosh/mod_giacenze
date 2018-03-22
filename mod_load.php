005d6bd26c858b7b38240fdf0cd7dcfcee8325c5
yiosh <sigue.nox@gmail.com>
2018-03-22 12:36:25 +0100

    Add new rows to table

--- mod_load.php
+++ mod_load.php
@@ -17,40 +17,30 @@ include("../../fl_inc/headers.php");?>
 <?php if(!isset($_GET['external'])) include('../../fl_inc/menu.php'); ?>
 <?php if(!isset($_GET['external'])) include('../../fl_inc/module_menu.php'); ?>
 
-<form name="" method="POST" class="ajaxForm">
+<div class="container">
+  <form name="" method="POST" class="ajaxForm">
+    <div class="dati-wrapper">
+      <table id ="dati" class="dati" summary="Dati">
+        <tr>
+          <th>Codice</th>
+          <th>Descrizione</th>
+          <th>UM</th>
+          <th>QTY</th>
+          <th>Prezzo</th>
+          <th>Sc. %</th>
+          <th>IVA</th>
+          <th>Importo</th>
+          <th></th>
+          <th></th>
+        </tr>
 
-<table class="dati" summary="Dati">
-  <tr>
-    <th>Codice</th>
-    <th>Descrizione</th>
-    <th>UM</th>
-    <th>QTY</th>
-    <th>Prezzo</th>
-    <th>Sc. %</th>
-    <th>IVA</th>
-    <th>Importo</th>
-    <th></th>
-    <th></th>
-  </tr>
-  
-  
-  <tr id="R1">
-    <td><input type="text" name=""></td>
-    <td><input type="text" name=""></td>
-    <td><input type="text" name=""></td>
-    <td><input type="text" name=""></td>
-    <td><input type="text" name=""></td>
-    <td><input type="text" name=""></td>
-    <td><input type="text" name=""></td>
-    <td><input type="text" name=""></td>
-    <th></th>
-  </tr>
+      </table>
+    </div>
+ 
+    <i id="aggiungi" title="Aggiungi riga" class="fas fa-plus-circle"></i>
 
+    <input type="submit" value="Carica Voci" class="submit button">
 
-</table>
-
-<input type="button" name="" value="Carica Voci" class="button">
-
-</form>
+  </form>
+</div>
 <?php include("../../fl_inc/footer.php"); ?>
-
