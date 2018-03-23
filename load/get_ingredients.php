b8b8ead889465dc49f054ce44ceb6fbaad9114f1
yiosh <sigue.nox@gmail.com>
2018-03-23 17:05:17 +0100

    Fixed bugs on inserting

---
+++ load/get_ingredients.php
@@ -0,0 +1,29 @@
+<?php
+  // require_once('../../fl_core/autentication.php');
+  $host = "localhost";
+  $login = "root";
+  $pwd = "";
+  $db = "banquet_lacavallerizza";
+
+  $conn = mysqli_connect($host,$login,$pwd,$db);
+
+  // Check connection
+  if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+  }
+
+  $sql = "SELECT id, codice_fornitore, formato, unita_di_misura_formato, valore_di_conversione, prezzo_unitario, iva FROM fl_listino_acquisto";
+  $result = mysqli_query($conn, $sql);
+ 
+
+  if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+      $json[] = $row;
+    }
+    echo json_encode($json);
+  } else {
+    echo "0 resultati";
+  }
+
+  mysqli_close($conn);
--- mod_load.php
+++ mod_load.php
@@ -9,15 +9,26 @@ $multi_order = (isset($_GET['multi_order'])) ? check($_GET['multi_order']) : 0;
 
 $module_title = "Carica DDT n. ".check($parent_id);
 
+// <div id="content-confirm" class="modal">
+//         <div class="modal-content modal-confirm">
+    
+//         </div>
+//       </div>
+// <label for="">Codice/Descripzione</label>
+// <input type="text" name="" id="search">
 
+require_once("../../fl_inc/headers.php"); 
 
-include("../../fl_inc/headers.php");?>
+?>
 
 <?php if(!isset($_GET['external'])) include('../../fl_inc/testata.php'); ?>
 <?php if(!isset($_GET['external'])) include('../../fl_inc/menu.php'); ?>
 <?php if(!isset($_GET['external'])) include('../../fl_inc/module_menu.php'); ?>
 
 <div class="container">
+<div class="aggiungi">
+  <label id="aggiungi" for=""><i  title="Aggiungi riga" class="fas fa-plus-circle"></i> Aggiungi riga</label>
+</div>
   <form name="" method="POST" class="ajaxForm">
     <div class="dati-wrapper">
       <table id ="dati" class="dati" summary="Dati">
@@ -34,10 +45,27 @@ include("../../fl_inc/headers.php");?>
           <th></th>
         </tr>
 
+        <tr id="r1">
+          <td class="codice"><input class="codice-field" type="text" name="codice"></td>
+          <td class="descrizione"><input class="descrizione-field" type="text" name="descrizione"></td>
+          <td class="um">
+            <select class="um-field select2" name="" id="">
+              <option value="KG">KG</option>
+              <option value="LT">LT</option>
+              <option value="PZ">PZ</option>
+            </select>
+          </td>
+          <td class="qty"><input class="qty-field"type="number" step="1" min="0" name="qty"></td>
+          <td class="prezzo"><input class="prezzo-field" step="0.01" min="0" type="number" name="prezzo"></td>
+          <td class="sc"><input class="sc-field" type="text" name="sc"></td>
+          <td class="iva"><input class="iva-field" type="text" name="iva"></td>
+          <td class="importo"><input class="importo-field" step="0.01" min="0" type="number" name="importo"></td>
+          <th class="delete-row"></th>
+        </tr>
       </table>
     </div>
  
-    <i id="aggiungi" title="Aggiungi riga" class="fas fa-plus-circle"></i>
+    
 
     <input type="submit" value="Carica Voci" class="submit button">
 
